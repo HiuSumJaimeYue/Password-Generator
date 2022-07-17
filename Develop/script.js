19d696f5fc3c0cb5cbdb5cb227bdf0bb033bf787
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-07-17 01:43:31 -0400

    Handle Cancel/undefined passwordText

--- Develop/script.js
+++ Develop/script.js
@@ -48,9 +48,13 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  if (typeof password === "undefined"){
+    return;
+  }
+  else{
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 
 }
 
